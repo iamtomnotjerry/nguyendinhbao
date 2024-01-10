f8fb3221f971a3a70adef6b10ef8b38a9b0b80f0
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2024-01-11 06:28:25 +0700

    quote

--- app/components/Quote.tsx
+++ app/components/Quote.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Quote() {
   return (
-    <div>
-        <p>Your thought will become your action then gradually repeat will become your habit and in the end it will become your personality</p>
+    <div className='bg-white p-6 rounded-md shadow-md mb-8'>
+        <h1 className='text-2xl font-bold mb-4 text-gray-800'>Quote</h1>
+        <p>- Your thought will become your action then gradually repeat will become your habit and in the end it will become your personality</p>
     </div>
   )
 }
