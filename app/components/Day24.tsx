99bba0fbb0824dbb9e3417061ec39a7fab3f21ca
Tom <talama112335@gmail.com>
2024-01-22 21:09:16 +0700

    Update Day24.tsx

--- app/components/Day24.tsx
+++ app/components/Day24.tsx
@@ -5,51 +5,14 @@ export default function Day24() {
     <div className='bg-white p-6 rounded-md shadow-md mb-8'>
     <div className="space-y-4 text-gray-700">
     <h2 className='text-2xl font-bold mb-4 text-gray-800'>Day 24: Wed</h2>
-          <p>
-            <span className="font-bold">5AM:</span> Get Up
-          </p>
-          <p>
-            <span className="font-bold">5AM - 5:10AM:</span> Skincare
-          </p>
-          <p>
-            <span className="font-bold">5:10AM - 5:15AM:</span> Read the Goals
-          </p>
-          <p>
-            <span className="font-bold">5:15AM - 5:45AM:</span> Yoga
-          </p>
-          <p>
-            <span className="font-bold">5:45AM-6:30AM:</span> Breakfast
-          </p>
-          <p>
-            <span className="font-bold">6:30AM - 7AM:</span> House Work
-          </p>
-          <p>
-            <span className="font-bold">7PM - 12AM:</span>Flutter
-          </p>
-          <p>
-            <span className="font-bold">12AM - 1PM:</span> Have lunch meal 
-          </p>
-          <p>
-            <span className="font-bold">1PM - 3PM:</span>Flutter
-          </p>
-          <p>
-            <span className="font-bold">3AM - 4PM:</span> Dinner
-          </p>
-          <p>
-            <span className="font-bold">4PM - 9PM:</span> HangOut with girlfriend
-          </p>
-          <p>
-            <span className="font-bold">7:30PM - 8PM:</span> Take a shower
-          </p>
-          <p>
-            <span className="font-bold">9PM - 9:15PM:</span> Skincare
-          </p>
-          <p>
-            <span className="font-bold">9:15PM - 9:30PM:</span> Write Plan for new day + Read Goals
-          </p>
-          <p>
-            <span className="font-bold">9:30PM - 10PM:</span> Yoga
-          </p>
+          <p>KPI</p>
+          <p>1. Finish Flutter Tutorial For Beginners by tech with Tim</p>
+          <p>2. Finish 2 videos of YOGA</p>
+          <p>3. Drink 3 liters of water</p>
+          <p>4. 1000 push-ups</p>
+          <p>5. Run 3 kms</p>
+          <p>6. 2 leetcode questions</p>
+          <p>7. 2 Videos on Ted</p>
         </div>
     </div>
   )
