a7db04184e7f2c8edcd026e80416fcb0a0fb07ed
Tom <talama112335@gmail.com>
2024-01-22 18:16:34 +0700

    a

---
+++ app/components/Day23.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function Day22() {
+  return (
+    <div className='bg-white p-6 rounded-md shadow-md mb-8'>
+    <div className="space-y-4 text-gray-700">
+    <h2 className='text-2xl font-bold mb-4 text-gray-800'>Day 23: Tuesday</h2>
+          <p>
+            <span className="font-bold">5AM:</span> Get Up
+          </p>
+          <p>
+            <span className="font-bold">5AM - 5:10AM:</span> Skincare
+          </p>
+          <p>
+            <span className="font-bold">5:10AM - 5:15AM:</span> Read the Goals
+          </p>
+          <p>
+            <span className="font-bold">5:15AM - 5:45AM:</span> Yoga
+          </p>
+          <p>
+            <span className="font-bold">5:45AM-6:30AM:</span> Breakfast
+          </p>
+          <p>
+            <span className="font-bold">6:30AM - 7AM:</span> House Work
+          </p>
+          <p>
+            <span className="font-bold">7PM - 12AM:</span>Flutter
+          </p>
+          <p>
+            <span className="font-bold">12AM - 1PM:</span> Have lunch meal 
+          </p>
+          <p>
+            <span className="font-bold">1PM - 4:30PM:</span>Flutter
+          </p>
+          <p>
+            <span className="font-bold">4:30AM - 6PM:</span> Dinner
+          </p>
+          <p>
+            <span className="font-bold">6PM - 7:30PM:</span> WorkOut
+          </p>
+          <p>
+            <span className="font-bold">7:30PM - 8PM:</span> Take a shower
+          </p>
+          <p>
+            <span className="font-bold">8PM - 9PM:</span> Flutter
+          </p>
+          <p>
+            <span className="font-bold">9PM - 9:15PM:</span> Skincare
+          </p>
+          <p>
+            <span className="font-bold">9:15PM - 9:30PM:</span> Write Plan for new day + Read Goals
+          </p>
+          <p>
+            <span className="font-bold">9:30PM - 10PM:</span> Yoga
+          </p>
+        </div>
+    </div>
+  )
+}
